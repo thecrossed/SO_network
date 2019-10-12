99b4db2b2a596b52ead67620216ba2ecc13e0da3
thecrossed <36526042+thecrossed@users.noreply.github.com>
2019-10-12 22:26:45 +0200

    Update Question_56786655.sql

--- Question_56786655.sql
+++ Question_56786655.sql
@@ -6,3 +6,15 @@ FROM
 WHERE 
     Id   = @Id
  
+-----
+--DECLARE @Id int = ##QuestionId##
+
+/*SELECT p.Id, p.title, p.AcceptedAnswerId, p.Body, p.PostTypeId,
+        p.OwnerUserId, p.OwnerDisplayName, p.AnswerCount, p.CommentCount,
+        c.Text, c.UserId, c.UserDisplayName*/
+SELECT *
+FROM 
+    Posts p
+--JOIN Comments c ON p.Id = c.PostId
+WHERE 
+    p.Id   = 56786809
